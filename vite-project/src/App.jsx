1396ddcc9dc4da5f61f55109d663bb862c17f5c7
jemin <jeminkhachariya029@gmail.com>
2023-04-19 16:35:06 +0530

    aryan change

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <h1>Jemin</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates laboriosam nemo qui iste dignissimos cum deserunt similique? Expedita aliquam porro laborum fugit tempora, debitis et quam nam magni soluta! Nesciunt incidunt non odio ex labore tempora nihil perspiciatis assumenda quisquam! Repudiandae perspiciatis asperiores a ipsam ut quam incidunt magnam, cum dicta. Necessitatibus quos, deserunt ut, adipisci voluptas a reprehenderit illum nobis soluta odit reiciendis temporibus sint quia voluptatibus quisquam nam perspiciatis harum laborum ipsum, unde quod cum expedita? Incidunt, magni sunt neque quae tenetur est! Aliquid deleniti mollitia ab illo quam quas, ut cum rem voluptatem quos magni earum exercitationem!
+      <p>tfytfy
       </p>
     </div>
   )
